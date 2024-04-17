c3f5095d9e426389b84607907450b69f08f538b3
FatboundOmia <jean.betanocurt@omia.com.co>
2024-04-17 15:49:41 -0500

    ya tengo embebido mi chat

---
+++ document.py
@@ -0,0 +1,4 @@
+
+def document():
+    f = open('files\mobydick.txt', 'r')
+    return f.read()
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from langchain_google_genai import GoogleGenerativeAI, ChatGoogleGenerativeAI
+from IPython.display import Markdown
+from IPython.display import display
+import textwrap
+import google.generativeai as genai
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+import document
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 
 GOOGLE_API_KEY = 'AIzaSyB0hf-O1dIEyx09hmxvDYo0dwWH6O_WrLc'
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
 
 
-llm = ChatGoogleGenerativeAI(model='gemini-pro', google_api_key=GOOGLE_API_KEY, temperature=0.3, verbose=True)
 
-while True:
-    
-    pregunta = input("Pregunta:")
-    result = llm.invoke('{}'.format(pregunta))
-    print('=='*25)
-    print(result.content)
-    print('=='*25)
+text = document.document()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=200)
+texts = text_splitter.split_text(text)
+
+embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+
+vector_index = Chroma.from_texts(texts, embeddings).as_retriever()
+
+
+
+
